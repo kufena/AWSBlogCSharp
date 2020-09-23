b477e9cc4cfc06bcb44805b698d8679f1caf38ab
Andrew Douglas <potterboy67@outlook.com>
2020-09-23 12:52:19 +0100

    Add project files.

---
+++ AWSBlogCSharp/BlogPostsGetAll.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace AWSBlogCSharp
+{
+    class BlogPostsGetAll
+    {
+        public BlogPostsGetAll() { }
+
+        /// <summary>
+        /// A Lambda function to respond to HTTP Get methods from API Gateway
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>The API Gateway response.</returns>
+        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            context.Logger.LogLine("Get Request\n");
+
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = "All the posts from the fair.",
+                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+            };
+
+            return response;
+        }
+    }
+}
---
+++ AWSBlogCSharp/BlogPostsGetById.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace AWSBlogCSharp
+{
+    class BlogPostsGetById
+    {
+        public BlogPostsGetById() { }
+
+        /// <summary>
+        /// A Lambda function to respond to HTTP Get methods from API Gateway
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>The API Gateway response.</returns>
+        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            context.Logger.LogLine("Get Request\n");
+
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = "One Post To Rule Them All.",
+                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+            };
+
+            return response;
+        }
+    }
+}
