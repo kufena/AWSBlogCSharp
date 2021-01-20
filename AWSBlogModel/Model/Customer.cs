979986203ed1ff6ad9fefac42b87af72df00802c
Andrew Douglas <potterboy67@outlook.com>
2021-01-20 19:48:57 +0000

    Hmmm, not sure how much of this will survive - new pages/components for various aspects of the site but also fixing the fetchdata so that it kind-of works now.

---
+++ AWSBlogModel/Model/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWSBlogModel.Model
+{
+    class Customer
+    {
+    }
+}
---
+++ AWSBlogModel/Model/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWSBlogModel.Model
+{
+    public class State
+    {
+        BlogPostURL[] blogposts { get; set; }
+        BlogPostModel current { get; set; }
+        Customer customer { get; set; }
+    }
+}
