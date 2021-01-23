b92690512fced6665621c2f1f3554668a213fe79
Andrew Douglas <potterboy67@outlook.com>
2021-01-23 17:55:11 +0000

    Mucking about with the tokens we get from logging in, the fetch data now uses the id token to retrieve information.  Works ok - not on the content page yet though.  All dodgy as passes info via a static class.

---
+++ AWSBlogApp2/Statics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSBlogApp2
+{
+    public class Statics
+    {
+        public static string access_token { get; set; }
+        public static string id_token { get; set; }
+    
+        public static string refresh_token { get; set; }
+    }
+}
