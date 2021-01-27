288ba36c0ec92c002f29c2969da8ddb66b37f8a5
Andrew Douglas <potterboy67@outlook.com>
2021-01-27 15:08:45 +0000

    It turns out, using RootResourceId in the LambdaInvokePolicy in the InvokeAllRole was a mistake and using a Ref to the API is the right jumble of letters.

--- AWSBlogApp2/Statics.cs
+++ AWSBlogApp2/Statics.cs
@@ -16,10 +16,9 @@ namespace AWSBlogApp2
         public static AuthFlowResponse authResponse { get; set; }
         public static ImmutableCredentials creds { get; set; }
 
-        public const string poolID = "eu-west-2_CtEzDKKWR";
-        public const string clientID = "398v8proc8mei7m5mdq8vtjo3q";
-        public const string identityPoolID = "eu-west-2:5b2ed568-fa52-4376-b719-b74bcc698b92";
-        public const string apiURL = @"https://omufctn0kh.execute-api.eu-west-2.amazonaws.com/Prod/";
-        
+        public const string poolID = "eu-west-2_Evqd08jV3";
+        public const string clientID = "fe0d3ge8amg5f3stun2apdin5";
+        public const string identityPoolID = "eu-west-2:e7ae8ff9-4eb2-49e3-92f0-cfafcdc89355";
+        public const string apiURL = @"https://nocovzsnq6.execute-api.eu-west-2.amazonaws.com/Prod/";
     }
 }
